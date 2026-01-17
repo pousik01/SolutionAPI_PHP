1a996f61df4234e256e0acfa03da83a80442b6ea
pousik <nazir_magomedov06@mail.ru>
2026-01-18 00:04:57 +0300

    fix address to DB

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
-$host = 'localhost';
-$username = 'mysql';
-$password = 'mysql';
+$host = '127.0.1.22';
+$username = 'root';
+$password = '';
 $database = 'booksDB';
 
 // Создаем подключение
@@ -10,5 +10,7 @@ $connect = mysqli_connect($host, $username, $password, $database);
 // Проверяем подключение
 if (!$connect) {
     echo("Connection failed: " . mysqli_connect_error());
+}else{
+    echo "Успешное соединение";
 }
 ?>
